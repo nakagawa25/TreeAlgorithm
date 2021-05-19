9f8cc618f0b81fcfde356c7a34acfaf32dd6f303
Lucas Nakagawa <53760877+nakagawa25@users.noreply.github.com>
2021-05-18 23:31:40 -0300

    Upload manual do código completo.
    V0.1 do algoritmo finalizada e testada com sucesso.

---
+++ TestDev_Paradigma/Controller/TreeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestDev_Paradigma.Controller
+{
+    internal class TreeController
+    {
+        private Model.Root TreeRoot { get; set; }
+
+        internal TreeController()
+        {
+            TreeRoot = new Model.Root();
+        }
+
+        internal Model.Root GetTree(List<int> inputTree)
+        {
+            SetTreeRootValueAndIndex(inputTree);
+            SetTreeBranches(inputTree);
+            return TreeRoot;
+        }
+
+        private void SetTreeRootValueAndIndex(List<int> inputTree)
+        {
+            TreeRoot.RootValue = inputTree.Max();
+            TreeRoot.ArrayIndex = inputTree.IndexOf(TreeRoot.RootValue);
+        }
+
+        private void SetTreeBranches(List<int> inputTree)
+        {
+            for (int i = 0; i < TreeRoot.ArrayIndex; i++)
+                TreeRoot.LeftBranch.Add(inputTree[i]);
+            TreeRoot.LeftBranch = TreeRoot.LeftBranch.OrderByDescending(i => i).ToList();
+
+            for (int i = TreeRoot.ArrayIndex + 1; i < inputTree.Count; i++)
+                TreeRoot.RightBranch.Add(inputTree[i]);
+            TreeRoot.RightBranch = TreeRoot.RightBranch.OrderByDescending(i => i).ToList();
+        }
+    }
+}
